7ff91564293db8125f33809a53a39f26343d87e5
IamGilgamesh <rhinocadaver@gmail.com>
2023-02-07 12:04:09 -0700

    Fixed some things

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -211,7 +211,7 @@ KangaskhanEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 26, BITE
+	db 27, BITE
 	db 31, TAIL_WHIP
 	db 36, MEGA_PUNCH
 	db 41, LEER
@@ -380,14 +380,14 @@ NidoqueenEvosMoves:
 
 CuboneEvosMoves:
 ; Evolutions
-	db EV_LEVEL, 28, MAROWAK
+	db EV_LEVEL, 18, MAROWAK
 	db 0
 ; Learnset
-	db 25, LEER
-	db 31, FOCUS_ENERGY
-	db 38, THRASH
-	db 43, BONEMERANG
-	db 46, RAGE
+	db  5, LEER
+	db  9, FOCUS_ENERGY
+	db 13, THRASH
+	db 17, BONEMERANG
+	db 30, RAGE
 	db 0
 
 RhyhornEvosMoves:
@@ -897,7 +897,7 @@ FarfetchdEvosMoves:
 
 VenonatEvosMoves:
 ; Evolutions
-	db EV_LEVEL, 31, VENOMOTH
+	db EV_LEVEL, 31, BUTTERFREE
 	db 0
 ; Learnset
 	db 24, POISONPOWDER
@@ -1436,12 +1436,12 @@ VenomothEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 24, POISONPOWDER
-	db 27, LEECH_LIFE
-	db 30, STUN_SPORE
-	db 38, PSYBEAM
-	db 43, SLEEP_POWDER
-	db 50, PSYCHIC_M
+	db 11, POISONPOWDER
+	db 12, LEECH_LIFE
+	db 13, STUN_SPORE
+	db 20, PSYBEAM
+	db 30, SLEEP_POWDER
+	db 40, PSYCHIC_M
 	db 0
 
 DewgongEvosMoves:
@@ -1476,7 +1476,7 @@ CaterpieEvosMoves:
 
 MetapodEvosMoves:
 ; Evolutions
-	db EV_LEVEL, 10, BUTTERFREE
+	db EV_LEVEL, 10, VENOMOTH
 	db 0
 ; Learnset
 	db 0
@@ -1485,13 +1485,11 @@ ButterfreeEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 12, CONFUSION
-	db 15, POISONPOWDER
-	db 16, STUN_SPORE
-	db 17, SLEEP_POWDER
-	db 21, SUPERSONIC
-	db 26, WHIRLWIND
-	db 32, PSYBEAM
+	db 31, CONFUSION
+	db 32, SLEEP_POWDER
+	db 37, SUPERSONIC
+	db 41, WHIRLWIND
+	db 42, PSYBEAM
 	db 0
 
 MachampEvosMoves:
@@ -1669,9 +1667,11 @@ PersianEvosMoves:
 
 MarowakEvosMoves:
 ; Evolutions
+	db EV_LEVEL, 26, KANGASKHAN
 	db 0
 ; Learnset
 	db 25, LEER
+	db 27, MEGA_PUNCH
 	db 33, FOCUS_ENERGY
 	db 41, THRASH
 	db 48, BONEMERANG
